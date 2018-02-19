9ff88f9b8cae63bc77df84147020b6fb7f2a48af
童 刘 <child1993@hotmail.com>
2018-02-19 17:25:50 +1100

    添加项目文件。

---
+++ Xiaohu/Controllers/FilesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Xiaohu.Models;
+
+namespace Xiaohu.Controllers
+{
+    public class FilesController : Controller
+    {
+        private XiaohuDBEntities db = new XiaohuDBEntities();
+
+        // GET: Files
+        public ActionResult Index()
+        {
+            var files = db.Files.Include(f => f.User);
+            return View(files.ToList());
+        }
+
+        // GET: Files/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // GET: Files/Create
+        public ActionResult Create()
+        {
+            ViewBag.UpUserName = new SelectList(db.Users, "UserName", "LastName");
+            return View();
+        }
+
+        // POST: Files/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,UpDate,FileName,UpUserName,AuditUserName,Format,Approved,Comment")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Files.Add(file);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UpUserName = new SelectList(db.Users, "UserName", "LastName", file.UpUserName);
+            return View(file);
+        }
+
+        // GET: Files/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UpUserName = new SelectList(db.Users, "UserName", "LastName", file.UpUserName);
+            return View(file);
+        }
+
+        // POST: Files/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,UpDate,FileName,UpUserName,AuditUserName,Format,Approved,Comment")] File file)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(file).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UpUserName = new SelectList(db.Users, "UserName", "LastName", file.UpUserName);
+            return View(file);
+        }
+
+        // GET: Files/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = db.Files.Find(id);
+            if (file == null)
+            {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: Files/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            File file = db.Files.Find(id);
+            db.Files.Remove(file);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Xiaohu/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Xiaohu.MvcApplication" Language="C#" %>
